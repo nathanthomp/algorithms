5205ca96d4a7f3668d9cd5a7fc2b7772b18f85ba
Nathan Thompson <thompn18@email.franklin.edu>
2024-09-12 17:12:32 -0400

    Chase Questions

--- Algorithms/src/Chase/QuestionOne.java
+++ Algorithms/src/Chase/QuestionOne.java
@@ -4,10 +4,36 @@ import java.io.IOException;
 
 class Result {
     // Answer goes here
+    private static boolean hasSubString(String subString, String s) {
+        if (s.contains(subString)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static String removeSubString(String subString, String s) {
+        String result = "";
+        int startIndex = s.indexOf(subString);
+        int endIndex = subString.length() - 1 + startIndex;
+        result += s.substring(0, startIndex);
+        result += s.substring(endIndex + 1, s.length());
+        return result;
+    }
+
+    public static String getFinalString(String s) {
+        String result = s;
+        while (hasSubString("AWS", s)) {
+            result = removeSubString("AWS", s);
+        }
+
+        return result;
+    }
 }
 
 public class QuestionOne {
     public static void main(String[] args) throws IOException {
-        // Driver code goes here
+        String s = "AWSG";
+        String result = Result.getFinalString(s);
+        System.out.println(result);
     }
 }
--- Algorithms/src/Chase/QuestionTwo.java
+++ Algorithms/src/Chase/QuestionTwo.java
@@ -1,13 +1,53 @@
 package Chase;
 
 import java.io.IOException;
+import java.util.*;
 
 class Result {
-    // Answer goes here
+    private static int[][] getTasksStartAndEndByMachine(List<Integer> start, List<Integer> end) {
+        int numberOfMachines = start.size();
+        int[][] result = new int[numberOfMachines][2]; // Tasks -> machineNum x Start/End
+
+        for (int i = 0; i < numberOfMachines; i++) {
+            // Row 0 is start, row 1 is end
+            result[i][0] = start.get(i);
+            result[i][1] = end.get(i);
+        }
+
+        return result;
+    }
+
+    public static int getMinMachines(List<Integer> start, List<Integer> end) {
+
+        // The min task for each machine and run on that machine
+        int[][] tasks = getTasksStartAndEndByMachine(start, end);
+
+        // Sort the tasks by machine (min)
+        Arrays.sort(tasks, (a, b) -> a[0] - a[1]);
+
+        // Store task end times
+        PriorityQueue<Integer> taskEndTimes = new PriorityQueue<Integer>();
+
+        //
+        for (int[] task : tasks) {
+
+            // Check if earliest task is finished
+            if (!taskEndTimes.isEmpty() && taskEndTimes.peek() < task[0]) {
+                taskEndTimes.poll();
+            }
+
+            taskEndTimes.add(task[1]);
+        }
+
+        return taskEndTimes.size();
+    }
 }
 
 public class QuestionTwo {
     public static void main(String[] args) throws IOException {
-        // Driver code goes here
+
+        int[] start = { 1, 2, 3, 4 };
+        int[] end = { 1, 2, 3, 4 };
+        System.out.println(getMinMachines(start, end));
     }
 }
